d6489c5fb55d05ef99404a1c48af3ed5e776e2e9
facemanguy <john32meyer@gmail.com>
2019-06-02 15:22:54 -0700

    Set up inital template for api call

--- src/views/CardFinder.vue
+++ src/views/CardFinder.vue
@@ -1,15 +1,20 @@
 <template>
     <div class="finder">
         <h1>Magic: The Gathering Card Finder</h1>
-        <form>
+        <form v-on:submit.prevent="findCards">
             <p>Look for a card by filling out the form below</p>
             <ul>
-                <li><label>Name <input type="text" placeholder="Nissa"></label></li>
+                <li><label>Name <input v-model="cardName" placeholder="Nissa"></label><button type="submit">Go</button></li>
                 <li><label>Color <input type="text" placeholder="Green"></label></li>
                 <li><label>Type <input type="text" placeholder="Creature"></label></li>
             </ul>
-            <p>{{}}</p>
+            
         </form>
+        <ul v-if="results.length > 0" class="results">
+            <li v-for="(card, index) in results.list" :key="index">
+            <p>{{results.name}}</p>
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -17,15 +22,17 @@
     import axios from 'axios';
 
     export default {
-        name: "CardFinder",
+        name: "finder",
         data () {
             return {
-
+                results: [],
+                errors: [],
+                cardName: ''
             }
         },
         methods: {
             findCards: function () {
-                this.results = null;
+                //this.results = null;
                 axios.get('https://api.magicthegathering.io/',{
                     params: {
                         name: this.cardName
@@ -36,7 +43,7 @@
                 })
                 .catch(error =>{
                     this.errors.push(error)
-                })
+                });
 
             }
         }
