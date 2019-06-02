d43f129a05d4f1a384fb95ed321a05d261ff5490
facemanguy <john32meyer@gmail.com>
2019-06-01 19:12:31 -0700

    Deleted Hello World, Routed CardFinder

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,15 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CardFinder from './views/CardFinder.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
-  }
 }
 </script>
 
--- src/views/CardFinder.vue
+++ src/views/CardFinder.vue
@@ -1,12 +1,18 @@
 <template>
-    <div>
-
+    <div class="finder">
+        <h1>Magic: The Gathering Card Finder</h1>
     </div>
 </template>
 
 <script>
     export default {
-        
+        name: "CardFinder",
+        data () {
+            //Fill Later
+        },
+        methods: {
+            //fill later
+        }
     }
 </script>
 
